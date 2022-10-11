3ecb2a8b6b404ec9151f8197a56e77848b4aa54d
annikatoivonen <annika.97@windowslive.com>
2022-10-11 17:33:12 +0300

    tekstin puhuminen toimii

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import * as Speech from 'expo-speech';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [thingToSay, setThingToSay] = useState('');
+
+  const speak = () => {
+    Speech.speak(thingToSay);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+      style={styles.input}
+      placeholder="Type text here..."
+      onChangeText={text => setThingToSay(text)}
+      value={thingToSay}></TextInput>
+      <Button title="PRESS TO HEAR TEXT" onPress={speak} />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +31,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    margin: 12,
+    padding: 10,
+    width: 300,
+  }
 });
